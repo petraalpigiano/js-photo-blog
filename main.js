dda5a62bcdc6a8a492de1388854fb9ae473d8846
petraalpigiano <petraalpigiano@gmail.com>
2025-05-04 17:45:42 +0200

    fix logic

--- main.js
+++ main.js
@@ -3,14 +3,19 @@
 // https://lanciweb.github.io/demo/api/pictures/
 // In ogni caso studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come poterli sfruttare.
 //Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+// 1° creo la lista su HTML con bootstrap
+// 2° copio la struttura e la cancello da HTML
+// 3° document.getelemetnbyID ("ul")
+// 4° ul.innerHTML e metto il pezzo di struttura con solo un item <li>
+// 5° nella <li> metto una variabile di appoggio vuota
+// 6° ogni email in pratica viene stampata come elemento di una lista perche riassengno la variabile ed aggiungo un pezzo di lista
+// 7° quindi mi serve un for o uso quello di sopra mettendolo dentro una funzione
 const rowEl = document.getElementById("row");
 const iconEl = document.getElementById("close-icon");
 const overlayEl = document.getElementById("overlay");
 const openImgEl = document.getElementById("open-image");
-// const imgContainerEl = document.getElementById("img-container");
 
 let card = ``;
-// let img = ``;
 
 axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((response) => {
   const JSONArray = response.data;
@@ -23,7 +28,8 @@ axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((response) => {
           alt="pin"
         />
         <div id="full-card" class="card h-100 rounded-0">
-          <img id="img-card"
+          <img
+            id="img-${element.id}"
             src="${element.url}"
             class="card-img-top px-3 pt-3"
             alt="${element.title}"
@@ -37,19 +43,62 @@ axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((response) => {
     rowEl.innerHTML = card;
   }
 
-  const cardNodes = document.querySelectorAll("#full-card");
-  console.log(cardNodes);
-  for (const node of cardNodes) {
-    console.log(node);
-    node.addEventListener("click", function () {
+  const cardNodeList = document.querySelectorAll("#full-card");
+  console.log(cardNodeList);
+  for (const currentNode of cardNodeList) {
+    console.log(currentNode);
+    currentNode.addEventListener("click", function () {
       overlayEl.classList.remove("d-none");
-      const nodeImg = node.querySelector("img");
-      opneImgEl.src = nodeImg.src;
+      displayCard(currentNode);
+      disappearBkCard(currentNode);
+    });
+    iconEl.addEventListener("click", function () {
+      overlayEl.classList.add("d-none");
+      reappearsBkCard(currentNode);
     });
   }
 });
 
-// OVERLAY SCOMPARE QUANDO CLICCO L'X
-iconEl.addEventListener("click", function () {
-  overlayEl.classList.add("d-none");
-});
+// OTHER SOLUTION
+// for (let i = 0; i < 6; i++) {
+//   axios
+//     .get(`https://lanciweb.github.io/demo/api/pictures/`)
+//     .then((response) => {
+//       const currentIndex = response.data[i];
+//       const currentTitle = currentIndex.title.toUpperCase();
+//       const currentImage = currentIndex.url;
+//       const currentDate = currentIndex.date;
+//       for (let i = 0; i < 1; i++) {
+//         card += `
+//   <div class="col position-relative">
+//     <img
+//       src="./img/pin.svg"
+//       class="position-absolute top-0 start-50 translate-middle z-1"
+//       alt="pin"
+//     />
+//     <div id="full-card" class="card h-100 rounded-0">
+//       <img
+//         src="${currentImage}"
+//         class="card-img-top px-3 pt-3"
+//         alt="${currentTitle}"
+//       />
+//       <div class="card-body">
+//         <time datetime="2024-07-01">${currentDate}</time>
+//         <h5 class="card-title">${currentTitle}</h5>
+//       </div>
+//     </div>
+//   </div> `;
+//       }
+//       rowEl.innerHTML = card;
+//     });
+// }
+// PUOI PRENDERE SOLO L'URL DELLE FOTO DALL'ENDPOINT
+// for (let i = 0; i < 6; i++) {
+//   const currentUrl = response.data[i].url;
+//   console.log(currentUrl);
+// }
+// PRENDI L'ID
+// const currentId = currentImg.id;
+// openImgEl.src = `https://marcolanci.it/boolean/assets/pictures/${currentId}.png`;
+// LEGGO L'ID DELL'IMMAGINE CORRENTE
+// console.log(currentImg.getAttribute("id"));
